9091140f03b94e9cda369ea4b512a00be37f8741
Vlad <vlad41700.konoplenco@gmail.com>
2025-11-19 16:38:59 +0200

    DI added

--- src/app/shared/components/items-list/items-list.ts
+++ src/app/shared/components/items-list/items-list.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ItemCardComponent } from '../item-card/item-card';
 import { Product } from '../core/product';
+import { Data } from '../../../services/data';
 
 @Component({
   selector: 'app-items-list',
@@ -11,66 +12,17 @@ import { Product } from '../core/product';
   templateUrl: './items-list.html',
   styleUrls: ['./items-list.css']
 })
-export class ItemsListComponent {
+export class ItemsListComponent implements OnInit {
+  
+  products: Product[] = [];   
+ 
+  constructor(private dataService: Data) { }
+
+  ngOnInit(): void {
+    this.products = this.dataService.getItems();
     
-  products: Product[] = [
-    {
-        id: 1,
-        name: 'Класичний костюм',
-        price: 5999,
-        category: 'men',
-        description: 'Елегантний чоловічий костюм темно-синього кольору',
-        image: {
-            url: 'suit.jpg',
-            alt: 'Класичний чоловічий костюм',
-            width: 300,
-            height: 400
-        },
-        isNew: true
-    },
-    {
-        id: 2,
-        name: 'Вечірня сукня',
-        price: 3999,
-        category: 'women',
-        description: 'Довга вечірня сукня з мереживом',
-        image: {
-            url: 'dress.jpg',
-            alt: 'Вечірня сукня',
-            width: 300,
-            height: 400
-        },
-        isNew: true
-    },
-    {
-        id: 3,
-        name: 'Шкіряна куртка',
-        price: 4599,
-        category: 'men',
-        description: 'Стильна шкіряна куртка чорного кольору',
-        image: {
-            url: 'leather-jacket.jpg',
-            alt: 'Шкіряна куртка',
-            width: 300,
-            height: 400
-        },
-        isNew: false
-    },
-    {
-        id: 4,
-        name: 'Кашемірове пальто',
-        price: 6999,
-        category: 'women',
-        description: 'Елегантне пальто з кашеміру бежевого кольору',
-        image: {
-            url: 'coat.jpg',
-            alt: 'Кашемірове пальто',
-            width: 300,
-            height: 400
-        },
-        isNew: true
-    }
-  ];
+    console.log('Дані успішно завантажено з сервісу:', this.products);
+  }
 
   searchTerm: string = '';
   originalProducts: Product[] = [...this.products];
